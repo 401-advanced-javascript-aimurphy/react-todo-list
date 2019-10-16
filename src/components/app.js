ccd8d06bbceeb4e71958401d3f2fe2f6728fddf7
Aileen Murphy <ai.murf2@gmail.com>
2019-10-15 18:35:15 -0700

    saving my place

--- src/components/app.js
+++ src/components/app.js
@@ -15,7 +15,7 @@ class App extends React.Component{
 
    
   addTask=(item)=>{
-    // console.log('form data is,', formData);
+    console.log('form data is,', item);
     // let item = formData;
     const newItem = {
       name: item,
@@ -52,16 +52,26 @@ class App extends React.Component{
     console.log(status[0].complete);
     status[0].complete=!status[0].complete;
     console.log(status);
+
+    let tasklist = this.state.tasks.filter((item) => item.id !== tog);
+    console.log(tasklist, 'is task list');
+    this.setState({tasks: tasklist});
+
+    // this.setState({item: status[0]});
+
+    this.setState({tasks: [...this.state.tasks, status[0]]});
+
+
   }
   // toggling? {item:[...item, complete:!item.complete]});
 
   // we are just setting state and updating the master list. 
   render(){return (
-    <div className="App">
+    <div className="todo">
       if(this.state.tasks.length>0){<Header />}
       
       <Form action={this.addTask}/>
-      <List className="todo" action={this.toggleComplete} tasks={this.state.tasks} buttons={this.deleteTask}>
+      <List  action={this.toggleComplete} tasks={this.state.tasks} buttons={this.deleteTask}>
       </List>
     </div>
   );}
